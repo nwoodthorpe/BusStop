5b0bbb86cd2cbb863fb096bf4b4d9aa2b9075df5
Harry-L <harryliu0@hotmail.com>
2016-04-22 18:28:14 -0700

    iOS

---
+++ iOS/BusStop/BusStop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BusStop
+//
+//  Created by Harry Liu on 2016-04-22.
+//  Copyright © 2016 HarryLiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
