7e4c37c5fc999c67b0b020ba68f07baa0a3bfd22
Harry-L <harryliu0@hotmail.com>
2016-04-25 22:57:00 -0700

    iOS Layout

---
+++ iOS/BusStop/BusStop/FinishedViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FinishedViewController.swift
+//  BusStop
+//
+//  Created by Harry Liu on 2016-04-25.
+//  Copyright © 2016 HarryLiu. All rights reserved.
+//
+
+import UIKit
+
+class FinishedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
